ccb21dd884e4bf87f091997c559bba602d15bf61
mateusz.gawronski <mateusz.gawronski@websensa.com>
2024-02-08 15:27:10 +0100

    add file

---
+++ custom_code.js
@@ -0,0 +1,20 @@
+console.log("swiper custom code");
+const teamSliderMarketing = new Swiper(".swiper.is-team", {
+  // Optional parameters
+  slidesPerView: 4,
+  spaceBetween: 16,
+  loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+  // And if we need scrollbar
+});
